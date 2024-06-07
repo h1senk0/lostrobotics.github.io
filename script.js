c16e2927952ec212c2c258c5723671ff78f24d5b
Nicolas Ficnar <78841948+h1senk0@users.noreply.github.com>
2024-06-07 20:49:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var x = setInterval(function() {
     var distance = countDownDate - now;
 
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)); 
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
